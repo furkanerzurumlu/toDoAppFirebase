0380e036a4f3341570ed07ae6f3bd3367c16ad06
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-08-23 16:28:47 +0300

    firebase connection

--- toDoAppFirebase/Commons/HomePage/ViewController.swift
+++ toDoAppFirebase/Commons/HomePage/ViewController.swift
@@ -6,11 +6,19 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var ref: DatabaseReference!
+        
+        
+        ref = Database.database().reference()
+        
+        
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addToDo))
         navigationController?.navigationBar.topItem?.rightBarButtonItem?.tintColor = UIColor.black
