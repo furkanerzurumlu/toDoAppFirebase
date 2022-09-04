97622a16c7788dc0217e23307e102165f3c3ece4
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-09-04 19:51:43 +0300

    change UI

--- toDoAppFirebase/Commons/HomePage/ViewController.swift
+++ toDoAppFirebase/Commons/HomePage/ViewController.swift
@@ -9,24 +9,66 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var allTaskTableView: UITableView!
+    @IBOutlet weak var addTaskText: UITextField!
+    @IBOutlet weak var taskAddButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var ref: DatabaseReference!
+        setLayout()
         
         
-        ref = Database.database().reference()
+    }
+    
+    @IBAction func taskAddButton(_ sender: Any) {
         
+    }
+    
+    
+    func setLayout(){
         
-        
-        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addToDo))
         navigationController?.navigationBar.topItem?.rightBarButtonItem?.tintColor = UIColor.black
         self.title = "TO DO"
+        
+        taskAddButton.setTitle("Add Task", for: .normal)
+        taskAddButton.setTitleColor(.white, for: .normal)
+        taskAddButton.layer.cornerRadius = 15
+        taskAddButton.layer.borderWidth = 2
+        taskAddButton.layer.borderColor = UIColor.black.cgColor
+        
+        
+        let viewColor = hexStringToUIColor(hex: "#FCF8E8")
+        let buttonColor = hexStringToUIColor(hex: "94B49F")
+        let tableViewColor = hexStringToUIColor(hex: "DF7861")
+        
+        view.backgroundColor = viewColor
+        taskAddButton.backgroundColor = buttonColor
+        allTaskTableView.backgroundColor = tableViewColor
+        
     }
-    @objc func addToDo(){
-        Router.shared.showAddMissionsVC(navigationController: self.navigationController)
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
-
 }
 
